9964f4df3b11cad0cc62814435d6415676c9a698
Umar Muzammil <umarmuzammil@hotmail.com>
2017-07-28 19:20:41 +0500

    updates

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform player; 
+	Vector3 Offset;
+	void Start()
+	{
+		Offset = player.position - transform.position;
+
+	}
+	void LateUpdate()
+	{
+		transform.position = Vector3.Lerp (transform.position, player.position - Offset, Time.deltaTime);
+	}
+
+}
